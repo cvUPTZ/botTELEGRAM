8f0580f6baddf03a263479253111f74fcee8950f
cvUPTZ <cvupdz@gmail.com>
2024-10-16 10:33:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,17 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-# Telegram Bot Token
-BOT_TOKEN = "7495077361:AAGFA9GN6MCoUjNmiWDEUwa7IvN8C7E1dR0"
-
-# Email configuration
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 SMTP_SERVER = os.getenv('SMTP_SERVER')
-SMTP_PORT = int(os.getenv('SMTP_PORT', 587))  # Default to 587 if not specified
-PORT =  int(os.getenv('PORT', 5000))
-
-WEBHOOK_URL = "https://bottelegram-wouf.onrender.com/webhook"
-# Other configuration variables
+SMTP_PORT = int(os.getenv('SMTP_PORT', 587))
+PORT = int(os.getenv('PORT', 5000))
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-# Add these variables if they're used in your code
 CV_FILES = {
     'junior': os.getenv('JUNIOR_CV_FILE'),
     'senior': os.getenv('SENIOR_CV_FILE')
@@ -24,6 +19,4 @@ CV_FILES = {
 QUESTIONS_FILE = os.getenv('QUESTIONS_FILE')
 SENT_EMAILS_FILE = os.getenv('SENT_EMAILS_FILE')
 SCRAPED_DATA_FILE = os.getenv('SCRAPED_DATA_FILE')
-
-# Convert admin_user_ids to a list of integers
 admin_user_ids = [int(id.strip()) for id in os.getenv('ADMIN_USER_IDS', '').split(',') if id.strip()]
