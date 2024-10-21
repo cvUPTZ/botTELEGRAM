851a84e4dbc2a5d0697ed4d34c54638f114ea865
cvUPTZ <cvupdz@gmail.com>
2024-10-21 20:14:41 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,6 +7,8 @@ import requests
 from jwt import PyJWKClient
 import jwt
 import json
+from utils.linkedin_utils import exchange_code_for_tokens, check_follow_status
+from utils.email_utils import send_email_with_cv
 import redis
 
 app = Flask(__name__)
