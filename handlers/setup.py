5f6c00e3eb6d820cd4516c2b26be9d499d4e9cc9
cvUPTZ <cvupdz@gmail.com>
2024-11-15 12:57:51 +0100

    Update setup.py

--- handlers/setup.py
+++ handlers/setup.py
@@ -1,6 +1,6 @@
 # handlers/setup.py
 from telegram.ext import CommandHandler, MessageHandler, filters
-from .user_handlers import start, ask_question, send_cv, my_id
+from .user_handlers import start, send_cv, my_id
 from .admin_handlers import liste_questions, tag_all, offremploi
 from .message_handlers import welcome_new_member, handle_message
 
@@ -8,8 +8,8 @@ async def setup_application(app):
     """Setup all handlers for the application"""
     # Command handlers
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("question", ask_question))
-    app.add_handler(CommandHandler("liste_questions", liste_questions))
+    # app.add_handler(CommandHandler("question", ask_question))
+    # app.add_handler(CommandHandler("liste_questions", liste_questions))
     app.add_handler(CommandHandler("sendcv", send_cv))
     app.add_handler(CommandHandler("myid", my_id))
     app.add_handler(CommandHandler("tagall", tag_all))
